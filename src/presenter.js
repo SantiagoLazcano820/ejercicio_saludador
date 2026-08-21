dffb55dc720368a0f9c170edbfd23821219ef735
Santiago Lazcano Quiroga <santiagolazcanoquiroga@MacBook-Air-de-Santiago.local>
2026-08-21 16:45:17 -0400

    feat: Agregar saludar por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,12 @@
-import sumar from "./sumador";
+import saludar from "./saludador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const nombre = document.querySelector("#nombre");
+const form = document.querySelector("#saludador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  div.innerHTML = "<p>" + saludar(nombre.value) + "</p>";
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
 });
---
+++ src/saludador.js
@@ -0,0 +1,8 @@
+function saludar(nombre = "") {
+  if (nombre) {
+    return "Hola " + nombre;
+  } 
+  return "Hola";
+}
+
+export default saludar;
