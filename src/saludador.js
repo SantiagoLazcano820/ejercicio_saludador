3c9f218e0eda337c0ea7c99c5ae0e2b2729a2c5b
Santiago Lazcano Quiroga <santiagolazcanoquiroga@MacBook-Air-de-Santiago.local>
2026-08-22 00:50:56 -0400

    feat:Agregar saludar en ingles

--- src/saludador.js
+++ src/saludador.js
@@ -1,6 +1,32 @@
 function saludar(nombre = "", hora = 0, genero = "", edad = 0, idioma = "") {
-  let prefijo = "";
+  const idiomaMayuscula = idioma.toUpperCase();
   const generoMayuscula = genero.toUpperCase();
+  if (idiomaMayuscula === "ENG") {
+    let prefijo = "";
+    if (edad >= 30) {
+      if (generoMayuscula === "M") {
+        prefijo = "Mr. ";
+      } else if (generoMayuscula === "F") {
+        prefijo = "Ms. ";
+      }
+    }
+
+    let saludo = "Hello";
+    if (hora >= 6 && hora < 12) {
+      saludo = "Good Morning";
+    } else if (hora >= 12 && hora < 19) {
+      saludo = "Good Afternoon";
+    } else {
+      saludo = "Good Night";
+    }
+
+    if (nombre) {
+      return saludo + ", " + prefijo + nombre;
+    }
+    return saludo;  
+  }
+  
+  let prefijo = "";
   if (edad >= 30) {
     if (generoMayuscula === "M") {
       prefijo = "Sr. ";
